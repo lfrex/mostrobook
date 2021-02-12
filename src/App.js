e6c0382187aa6fad26857e618fde906629b4d6a3
villalpando <villalpandooctavio@johndeere.com>
2021-02-12 13:37:16 -0600

    FriendsPage working w/Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import axios from 'axios';
 import Profile from './components/Profile';
 import FriendsPage from './components/FriendsPage';
 
@@ -15,18 +16,37 @@ class App extends Component {
         image: "https://i.pinimg.com/564x/68/ee/c7/68eec72db45045c5e5d3eee08e992531.jpg",
         friendList: ["Leo", "Raul", "Karen"]
       },
-      apiDataLoaded: false,
-      potentialFriends: []
+      potentialFriends: [],
+      apiDataLoaded: false
     }
   }
 
+  componentDidMount = async () => {
+    const friendOne = await axios.get("https://randomuser.me/api/");
+    const friendTwo = await axios.get("https://randomuser.me/api/");
+    const friendThree = await axios.get("https://randomuser.me/api/");
+    const friendFour = await axios.get("https://randomuser.me/api/");
+    const friendFive = await axios.get("https://randomuser.me/api/");
+
+    const friends = [friendOne.data.results[0], 
+    friendTwo.data.results[0],
+    friendThree.data.results[0],
+    friendFour.data.results[0],
+    friendFive.data.results[0] 
+    ];
+
+    this.setState({
+      potentialFriends: friends,
+      apiDataLoaded: true
+    })    
+  }
 
   render() {
     return (
       <div className="App">
         <h1>MostroBook</h1>
        <Profile user={this.state.user}/>
-       <FriendsPage />
+       <FriendsPage potentialFriends={this.state.potentialFriends}/>
       </div>
     );
   } 
--- src/components/FriendsPage.js
+++ src/components/FriendsPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const FriendsPage = (props) => {
+    console.log(props);
     return (
-       <p>Lorem</p> 
-    )
+   <div>
+       {props.potentialFriends.map(friend => (
+           <div key={friend.cell} >
+               <img src={friend.picture.thumbnail} />
+               <h3>{friend.name.first} {friend.name.last}</h3>
+               <p>{friend.login.username}</p>
+            </div>
+       ))}
+   </div>
+       )
 }
 
 export default FriendsPage;
